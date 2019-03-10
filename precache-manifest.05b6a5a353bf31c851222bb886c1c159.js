1b598f0c6bb64d81addbc2b06d4ee6bc8ba50f5e
BiYuqi <biyuqiwan@163.com>
2019-03-10 13:06:57 +0000

    Travis CI Auto Builder at 2019-03-10 13:06

---
+++ precache-manifest.05b6a5a353bf31c851222bb886c1c159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e805f7677e243e502a7",
+    "url": "/react-todolist/static/css/main.162e89bf.chunk.css"
+  },
+  {
+    "revision": "4e805f7677e243e502a7",
+    "url": "/react-todolist/static/js/main.4e805f76.chunk.js"
+  },
+  {
+    "revision": "d4769facc7c6b35f5822",
+    "url": "/react-todolist/static/js/1.d4769fac.chunk.js"
+  },
+  {
+    "revision": "15e18c78eb78a1dce817",
+    "url": "/react-todolist/static/js/runtime~main.15e18c78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "19f2cefb574bb38f5f2f12f80d1a92f6",
+    "url": "/react-todolist/index.html"
+  }
+];
